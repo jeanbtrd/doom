b86c8084c6d266d5d436ca49a0416f9aea4dd63a
jean <jeanbtrd@gmail.com>
2025-10-21 15:31:24 +0200

    autosave

--- config.el
+++ config.el
@@ -6,4 +6,9 @@
 
 (setq org-directory "~/mega/org/")
 
+;; Ensure autosave is active globally
+(setq auto-save-default t)
+(setq auto-save-interval 20)  ;; keystrokes before autosave
+(setq auto-save-timeout 2)    ;; seconds of idle before autosave
+
 (setq projectile-project-search-path ("~/mega/uni/EE/Tesi/journal/org" . 1))
